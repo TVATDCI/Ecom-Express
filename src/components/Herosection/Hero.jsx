85c22120cc2bb16fd0e45e79457e1588afa8c45a
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2024-12-25 23:25:24 +0100

    Cleaning up the mess

--- src/components/Herosection/Hero.jsx
+++ src/components/Herosection/Hero.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Button from "./Button";
 import Section from "./Section";
 import { useNavigate } from "react-router-dom";
@@ -19,12 +18,16 @@ const Hero = () => {
       }}
     >
       <div className="flex flex-col items-center bg-black bg-opacity-30 p-5 w-1/2">
-        <h2 className="text-3xl font-bold mb-3 " style={{color: 'rgb(245, 208, 144)'}}>"Unbeatable Black Friday Deals—Shop Now and Save Big!"</h2>
-        <p className="text-xl  mb-3" style={{color: 'rgba(176, 38, 55,0.8)'}} >
-        "Black Friday Exclusive: Up to 30% Off New Arrivals!"</p>
-        <Button onClick={handleClick}>
-          SHOP NOW
-        </Button>
+        <h2
+          className="text-3xl font-bold mb-3 "
+          style={{ color: "rgb(245, 208, 144)" }}
+        >
+          "Unbeatable Black Friday Deals—Shop Now and Save Big!"
+        </h2>
+        <p className="text-xl  mb-3" style={{ color: "rgba(176, 38, 55,0.8)" }}>
+          "Black Friday Exclusive: Up to 30% Off New Arrivals!"
+        </p>
+        <Button onClick={handleClick}>SHOP NOW</Button>
       </div>
     </Section>
   );
